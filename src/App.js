592945b6bbb46f18d2ce5e09aa62b2c4cf85f409
Manan Doshi <manandoshi1301@gmail.com>
2021-05-28 21:14:23 +0530

    Base nav created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Banner from './components/layout/banner/Banner';
+import Nav from './components/layout/nav/Nav';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Banner />      
+      <Nav />
     </div>
   );
 }
---
+++ src/components/layout/nav/Nav.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <div>
+      <nav id='nav' className=''>
+        <div className=''>
+          <a href='' className='my-2'>
+            Home
+          </a>
+          <a href='' className='my-2'>
+            About
+          </a>
+          <a href='' className='my-2'>
+            Projects
+          </a>
+          <a href='' className='my-2'>
+            Contact
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;
