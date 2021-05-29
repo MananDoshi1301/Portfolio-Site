21fd9565e5c863a8dfe7d5a613b7bdf35008709f
Manan Doshi <manandoshi1301@gmail.com>
2021-05-29 18:39:32 +0530

    Partial carousel complete

--- src/components/layout/work/WorkCarousel.js
+++ src/components/layout/work/WorkCarousel.js
@@ -4,8 +4,17 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a lo
 import './WorkCarousel.css';
 
 const WorkCarousel = () => {
+
+  const projects = [
+    {title:'Contact-Keeper',type:'Website',year:'2021',link:'',leftColor:'#1565C0',rightColor:'#b92b27', shape:'square'},
+    {title:'BlogIT',type:'Website',year:'2021',link:'',leftColor:'#11998e',rightColor:'#38ef7d', shape:'circle'},
+    {title:'Github-Finder',type:'Website',year:'2021',link:'',leftColor:'#23074d',rightColor:'#cc5333', shape:'square'},
+    {title:'News-App',type:'Website',year:'2021',link:'',leftColor:'#f5af19',rightColor:'#f12711', shape:'circle'},
+    {title:'My-Toy-Store',type:'Website',year:'2021',link:'',leftColor:'#c31432',rightColor:'#240b36', shape:'square'},
+    {title:'Virtual-Labs',type:'Website',year:'2020',link:'',leftColor:'#8E2DE2',rightColor:'#4A00E0', shape:'circle'},
+  ]
   const [currentSlide, setCurrentSlide] = useState(1);
-  const [totalSlides, setTotalSlides] = useState(3);
+  const [totalSlides, setTotalSlides] = useState(projects.length);
   const [progress, setProgress] = useState((1 / totalSlides) * 100);
   const carouselProps = {
     showThumbs: false,
@@ -20,26 +29,23 @@ const WorkCarousel = () => {
     // centerMode: true,
   };
 
-  const arrowSyles = {
+  const arrowStyles = {
     position: 'absolute',
     zIndex: 2,
-    top: 'calc(10% - 5px)',
-    height: 50,
-    width: 80,
+    top: 'calc(70%)',
+    // height: 50,
+    // width: 80,
     cursor: 'pointer',
   };
 
   const rightArrow = (onClickHandler, label) => {
     return (
       <div
-        style={{ ...arrowSyles, right: 15 }}
+        style={{ ...arrowStyles, right: '0' }}
         onClick={onClickHandler}
         title={label}
       >
-        <img
-          src='https://img.icons8.com/ios/50/4a90e2/long-arrow-right.png'
-          alt=''
-        />
+        <i className='fas fa-long-arrow-alt-right fa-5x arrow-icon'></i>
       </div>
     );
   };
@@ -47,11 +53,11 @@ const WorkCarousel = () => {
   const leftArrow = (onClickHandler, label) => {
     return (
       <div
-        style={{ ...arrowSyles, left: 15 }}
+        style={{ ...arrowStyles, left: '30px' }}
         onClick={onClickHandler}
         title={label}
       >
-        <img src='https://img.icons8.com/ios/50/4a90e2/long-arrow-left.png' />
+        <i className='fas fa-long-arrow-alt-left fa-5x arrow-icon'></i>
       </div>
     );
   };
@@ -61,6 +67,14 @@ const WorkCarousel = () => {
     setCurrentSlide(current + 1);
     setProgress(value);
   };
+
+  const getGradient = (leftCol, rightCol) => {
+    const gradientStyle={
+      background: `-webkit-linear-gradient(to left, ${leftCol}, ${rightCol})`,
+      background: `linear-gradient(to left, ${leftCol}, ${rightCol})`
+    }
+    return gradientStyle;
+  }
 
   return (
     <div>
@@ -74,42 +88,44 @@ const WorkCarousel = () => {
             slideChange(current);
           }}
         >
-          <div className='carouselDiv p-5 d-flex justify-content-between align-items-center'>
-            <div>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
-              quia dicta voluptatum ipsum corrupti repudiandae inventore
-              perspiciatis. Vel, laudantium obcaecati. Hic soluta saepe quisquam
-              non doloribus facere inventore quasi? Sunt?Lorem ipsum dolor sit
-              amet consectetur adipisicing elit. Itaque quia dicta voluptatum
-              ipsum corrupti repudiandae inventore perspiciatis. Vel, laudantium
-            </div>
-          </div>
-          <div className='carouselDiv p-5 d-flex justify-content-between align-items-center'>
-            <div>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
-              quia dicta voluptatum ipsum corrupti repudiandae inventore
-              perspiciatis. Vel, laudantium obcaecati. Hic soluta saepe quisquam
-              non doloribus facere inventore quasi? Sunt?Lorem ipsum dolor sit
-              amet consectetur adipisicing elit. Itaque quia dicta voluptatum
-              ipsum corrupti repudiandae inventore perspiciatis. Vel, laudantium
-            </div>
-          </div><div className='carouselDiv p-5 d-flex justify-content-between align-items-center'>
-            <div>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
-              quia dicta voluptatum ipsum corrupti repudiandae inventore
-              perspiciatis. Vel, laudantium obcaecati. Hic soluta saepe quisquam
-              non doloribus facere inventore quasi? Sunt?Lorem ipsum dolor sit
-              amet consectetur adipisicing elit. Itaque quia dicta voluptatum
-              ipsum corrupti repudiandae inventore perspiciatis. Vel, laudantium
-            </div>
-          </div>
+          {projects.map((project, key)=>{
+            return (
+              <div key={key} className='carouselDiv p-5 d-flex justify-content-between'>
+                <div className=''>
+                  <div id={`${project.shape}`} className='shape' style={getGradient(project.leftColor, project.rightColor)}></div>
+                  <div className='projectDetails'>
+                    <h1 className='projectDetails-title'>{project.title}</h1>
+                    <h3 className='projectDetails-type'>
+                      {project.type}, <span className='projectDetails-year'>{project.year}</span>
+                    </h3>
+                    <h6 className='projectDetails-link'>View Project</h6>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+                            
         </Carousel>
 
-        <div className='d-flex  justify-content-end align-items-center' >
+        <div className='d-flex  justify-content-end align-items-center'>
           <div>0{currentSlide}</div>
           <div className='carouselBar mx-3 d-flex align-items-center'>
-            <span className='carouselBar-span' style={{ background: 'linear-gradient(45deg, blue, red)', width: `${progress}%`, height: '3px' }}></span>
-            <span className='carouselBar-span' style={{ backgroundColor: 'gray', width: `${100 - progress}%`, height: '1px' }}></span>
+            <span
+              className='carouselBar-span'
+              style={{
+                background: 'linear-gradient(45deg, blue, red)',
+                width: `${progress}%`,
+                height: '3px',
+              }}
+            ></span>
+            <span
+              className='carouselBar-span'
+              style={{
+                backgroundColor: 'gray',
+                width: `${100 - progress}%`,
+                height: '1px',
+              }}
+            ></span>
           </div>
           <div>0{totalSlides}</div>
         </div>
