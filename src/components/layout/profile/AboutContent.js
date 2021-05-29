9cf369e8389fcf4628d70f421d3d45b82b08340e
MananDoshi1301 <manandoshi1301@gmail.com>
2021-05-29 13:20:18 +0530

    Some css updates, introduced gradient

--- src/components/layout/profile/AboutContent.js
+++ src/components/layout/profile/AboutContent.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const AboutContent = () => {
   return (
-    <div className='fs-3 d-flex justify-content-center'>
-      <div className='w-75'>
+    <div className='fs-3 d-flex justify-content-end'>
+      <div className='w-75 profile-content'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
         esse earum quasi, vitae enim dolorum repellat rem cum deleniti non nulla
         laboriosam consectetur inventore molestias consequuntur fugiat quibusdam
--- src/components/layout/profile/Profile.js
+++ src/components/layout/profile/Profile.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import AboutContent from './AboutContent';
 import AboutImage from './AboutImage';
+import './Profile.css'
 
 const Profile = () => {
     return (
-        <div className='container' style={{marginTop: '15rem'}}>
-            <h1 className='text-center display-1'>Hey I'm Manan!</h1>
-            <h2 className='text-center mb-5 fs-2'>~ This is About Me ~</h2>
+        <div className='container profile' style={{ marginTop: '15rem' }}>
+            <h1 className='text-center display-1 profile-head'>Hey I'm Manan!</h1>
             <div className="row mt-5">
-            <div className="col-lg-6 d-flex align-items-center">
-                <AboutContent />
-            </div>
-            <div className="col-lg-6">
-                <AboutImage />
-            </div>
+                <div className="col-lg-6 d-flex align-items-center profile-content">
+                    <AboutContent />
+                </div>
+                <div className="col-lg-6">
+                    <AboutImage />
+                </div>
             </div>
         </div>
     )
