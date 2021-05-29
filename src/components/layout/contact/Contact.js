c11d5677e7b906f8c7a6f1e22cde44baed51bbe8
Manan Doshi <manandoshi1301@gmail.com>
2021-05-30 03:39:29 +0530

    Created Contact Forms

---
+++ src/components/layout/contact/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div className='container'>
+            <div className="display-1 h1">Connect with Me!</div>
+            <div className="col-lg-6"></div>
+            <div className="col-lg-6"></div>
+        </div>
+    )
+}
+
+export default Contact
