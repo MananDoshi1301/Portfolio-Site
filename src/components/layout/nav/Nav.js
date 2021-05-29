876e98747ac55c4a77fbece0219540fe99c0d0e6
Manan Doshi <manandoshi1301@gmail.com>
2021-05-29 21:36:20 +0530

    nav icons designed successfully

--- src/components/layout/nav/Nav.js
+++ src/components/layout/nav/Nav.js
@@ -4,11 +4,11 @@ import './Nav.css'
 const Nav = () => {
   return (
     <div>
-      <nav className='sideNav d-flex flex-column justify-content-around'>
-        <a href="">Home</a>
-        <a href="">About</a>
-        <a href="">Work</a>
-        <a href="">Contact</a>
+      <nav className='sideNav d-flex flex-column justify-content-around nav-icon-wrap nav-icon-effect-1 nav-icon-effect-1a'>
+        <a href="" className='d-flex justify-content-center align-items-center nav-icon'><i class="fas fa-home fa-2x"></i></a>
+        <a href="" className='d-flex justify-content-center align-items-center nav-icon'><i class="fas fa-user fa-2x"></i></a>
+        <a href="" className='d-flex justify-content-center align-items-center nav-icon'><i class="fas fa-briefcase fa-2x"></i></a>
+        <a href="" className='d-flex justify-content-center align-items-center nav-icon'><i class="fas fa-phone-alt fa-2x"></i></a>
       </nav>
     </div>
   );
