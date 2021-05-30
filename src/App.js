27df6fd4ad0e71e4fb814538b9771e5ae3caabb5
MananDoshi1301 <manandoshi1301@gmail.com>
2021-05-30 10:58:39 +0530

    Imported contact

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import Banner from './components/layout/banner/Banner';
 import Nav from './components/layout/nav/Nav';
 import Profile from './components/layout/profile/Profile';
 import Work from './components/layout/work/Work'
+import Contact from './components/layout/contact/Contact';
 import './App.css';
 
 function App() {
   return (
     <div className="App mb-5">
       <Nav />
-      <Banner />      
+      <Banner />
       <Profile />
       <Work />
+      <Contact />
     </div>
   );
 }
