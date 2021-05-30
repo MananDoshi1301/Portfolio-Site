d6217973187191707651316f5b620ddb801b1a5f
Manan Doshi <manandoshi1301@gmail.com>
2021-05-30 19:59:31 +0530

    Logo in Nav added

--- src/components/layout/nav/Nav.js
+++ src/components/layout/nav/Nav.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Nav.css'
+import logo from '../../images/logo5.png'
 
 const Nav = () => {
   return (
     <div>
       <nav className='sideNav d-flex flex-column justify-content-around nav-icon-wrap nav-icon-effect-1 nav-icon-effect-1a'>
+        <div className='d-flex justify-content-center align-items-center nav-logo'><img src={logo} alt="" /></div>
         <a href="" className='d-flex justify-content-center align-items-center nav-icon'><i class="fas fa-home fa-2x"></i></a>
         <a href="" className='d-flex justify-content-center align-items-center nav-icon'><i class="fas fa-user fa-2x"></i></a>
         <a href="" className='d-flex justify-content-center align-items-center nav-icon'><i class="fas fa-briefcase fa-2x"></i></a>
