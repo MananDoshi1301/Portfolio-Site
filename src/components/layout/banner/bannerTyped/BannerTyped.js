8d8efe85453ac7fa44098fab7a9ec7bc2463a29e
MananDoshi1301 <manandoshi1301@gmail.com>
2021-05-31 12:12:38 +0530

    TypeSpeed and fs updated

--- src/components/layout/banner/bannerTyped/BannerTyped.js
+++ src/components/layout/banner/bannerTyped/BannerTyped.js
@@ -8,11 +8,11 @@ const BannerTyped = () => {
     <div className='bannertyped'>
       <div className='bannerTypedDiv w-75 text-center'>
         <Typed
-          strings={['Hey, I Am Manan!','Do You Know Me?', 'No?', 'Then It\'s Just A Matter<br> Of Scroll!']}
-          typeSpeed={50}
+          strings={['Hey, I Am Manan!', 'Do You Know Me?', 'No?', 'Then It\'s Just A Matter<br> Of Scroll!']}
+          typeSpeed={70}
           backSpeed={50}
-          backDelay={3000}
-          loop
+          backDelay={2000}
+          // loop
           smartBackspace
         />
       </div>
