7cf42953a2d4ca3d480221b2803b16f485bb7546
MananDoshi1301 <manandoshi1301@gmail.com>
2021-06-01 13:00:09 +0530

    aos added!

--- src/components/layout/work/Work.js
+++ src/components/layout/work/Work.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import WorkCarousel from './WorkCarousel';
 import './Work.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init();
 
 const Work = () => {
     return (
         <div className='container work'>
-            <h1 className='display-1 text-center work-head'>Wanna See my Work?!</h1>
+            <h1 className='display-1 text-center work-head' data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-duration='1000' data-aos-delay='500'>Wanna See my Work?!</h1>
             <WorkCarousel />
         </div>
     )
