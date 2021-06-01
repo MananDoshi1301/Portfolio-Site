fd4ecc8cea6bd38ca12138898535880d86f5513e
MananDoshi1301 <manandoshi1301@gmail.com>
2021-06-02 01:40:03 +0530

    Created Work Modal files

---
+++ src/components/layout/work/workmodal/WorkModal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./WorkModal.css";
+
+const WorkModal = () => {
+  return (
+    <div className='workModal contents'>
+
+    </div>
+  )
+}
+
+export default WorkModal
