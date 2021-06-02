2f2c42ea6f5f6cf84eaf5c79bb2f03c68da0a367
Manan Doshi <manandoshi1301@gmail.com>
2021-06-02 17:49:24 +0530

    updated css

--- src/components/layout/nav/Nav.js
+++ src/components/layout/nav/Nav.js
@@ -12,7 +12,7 @@ const Nav = () => {
   }
   return (
     <div>
-      <nav className='sideNav d-flex flex-column justify-content-around nav-icon-wrap nav-icon-effect-1 nav-icon-effect-1a'>
+      <nav className='sideNav d-flex flex-column justify-content-around align-items-center nav-icon-wrap nav-icon-effect-1 nav-icon-effect-1a'>
         <motion.div initial={initial} animate={animate} transition={{type:"spring", stiffness:30,delay:0}} className='d-flex justify-content-center align-items-center nav-logo'><img src={logo} alt="" /></motion.div>
         <motion.a initial={initial} animate={animate} transition={{type:"spring", stiffness:30,delay:0.5}} href="" className='d-flex justify-content-center align-items-center nav-icon'><i class="fas fa-home fa-2x"></i></motion.a>
         <motion.a initial={initial} animate={animate} transition={{type:"spring", stiffness:30,delay:1}} href="" className='d-flex justify-content-center align-items-center nav-icon'><i class="fas fa-user fa-2x"></i></motion.a>
