16f7ccd920bb71869f6108c0c05fb63acb2e5db6
Manan Doshi <manandoshi1301@gmail.com>
2021-06-13 23:35:41 +0530

    Added new font, star Img

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -1,28 +1,69 @@
 import React from 'react';
 import './Footer.css';
 import logo from '../../images/logo5.png';
-import AOS from "aos";
-import "aos/dist/aos.css";
+import star from '../../images/star.webp';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 AOS.init();
 
 const Footer = () => {
   return (
     <div className='container'>
       <div className='footer'>
-        <div className='logoDiv d-flex justify-content-between'>
-          <div className='container-fluid d-flex flex-column justify-space-between'>
-            <div className="display-1 footer-header">
-              <div data-aos="fade-right">
-                <div className='text-start'>Manan</div>
-                <div className='text-end'>Doshi</div>
+        <div className='logoDiv row'>
+          <div className='container-fluid col-lg-6 offset-lg-1'>
+            <div className='display-1 footer-header'>
+              <div data-aos='fade-right'>
+                <div className='d-flex jusify-content-between'>
+                  Manan
+                  <div className='d-flex justify-content-between align-items-center'>
+                    <img
+                      src={star}
+                      className='img-fluid star star-1 w-30 align-self-start'
+                      alt=''
+                    />
+                    <img
+                      src={star}
+                      className='img-fluid star star-2 w-30 align-self-center'
+                      alt=''
+                    />
+                    <img
+                      src={star}
+                      className='img-fluid star star-3 w-30 align-self-start'
+                      alt=''
+                    />
+                  </div>
+                </div>
+                <div className='text-end'>
+                  <span>
+                    {/* <i class="fab fa-twitter"></i>
+                    <i class="fab fa-linkedin-in"></i> */}
+                  </span>
+                  Doshi
+                </div>
               </div>
             </div>
-            <div className='copyrightDiv text-center mt-5' data-aos="fade-up" data-aos-delay='900'>Copyrights @ 2021 MananDoshi. All rights reserved.</div>
+            <div
+              className='copyrightDiv text-center mb-5'
+              data-aos='fade-down'
+              data-aos-delay='900'
+            >
+              Copyrights @ 2021 MananDoshi. All rights reserved.
+            </div>
           </div>
-          <img src={logo} alt='' className='footer-logo' data-aos="fade-left" data-aos-delay='900'/>
+          {/* <div className="col-lg-2"></div> */}
+          <div className='col-lg-4 offset-lg-1'>
+            <img
+              src={logo}
+              alt=''
+              className='footer-logo img-fluid'
+              data-aos='fade-left'
+              data-aos-delay='900'
+            />
+          </div>
         </div>
       </div>
-    </div >
+    </div>    
   );
 };
 
