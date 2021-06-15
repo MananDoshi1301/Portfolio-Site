a8af3b108c55dc5e1f8811c008a52fff145bd84f
Manan Doshi <manandoshi1301@gmail.com>
2021-06-15 18:01:57 +0530

    Updated with new content

--- src/components/layout/profile/AboutContent.js
+++ src/components/layout/profile/AboutContent.js
@@ -7,11 +7,8 @@ AOS.init();
 const AboutContent = () => {
   return (
     <div className='fs-3 d-flex justify-content-end' data-aos='fade-right' data-aos-delay='900' data-aos-duration='1000'>
-      <div className='w-75 about-content'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
-        esse earum quasi, vitae enim dolorum repellat rem cum deleniti non nulla
-        laboriosam consectetur inventore molestias consequuntur fugiat quibusdam
-        beatae harum!
+      <div className='w-80 about-content'>
+        Enthusiastic about Web Architectures, Machine Learning, and Blockchain Technologies, I am currently pursuing a Bachelor's of Technology in Information Technology. Looking forward to gaining industry-level experience. 
       </div>
     </div>
   );
