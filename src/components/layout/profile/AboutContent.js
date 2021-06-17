b8e4377aec6162e719ceb77ef59fb0954834bf1f
Manan Doshi <manandoshi1301@gmail.com>
2021-06-17 23:52:21 +0530

    New content added and css updated

--- src/components/layout/profile/AboutContent.js
+++ src/components/layout/profile/AboutContent.js
@@ -1,14 +1,37 @@
 import React from 'react';
-import './AboutContent.css'
-import AOS from "aos";
-import "aos/dist/aos.css";
+import './AboutContent.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 AOS.init();
 
 const AboutContent = () => {
   return (
-    <div className='fs-3 d-flex justify-content-end' data-aos='fade-right' data-aos-delay='900' data-aos-duration='1000'>
-      <div className='w-80 about-content'>
-        Enthusiastic about Web Architectures, Machine Learning, and Blockchain Technologies, I am currently pursuing a Bachelor's of Technology in Information Technology. Looking forward to gaining industry-level experience. 
+    <div
+      className='fs-4 d-flex justify-content-end h-75'
+      data-aos='fade-right'
+      data-aos-delay='900'
+      data-aos-duration='1000'
+    >
+      <div className='w-80 about-content d-flex flex-column justify-content-around'>
+        <div>
+          Enthusiastic about Web Architectures, Machine Learning, and Blockchain
+          Technologies, I am currently pursuing a Bachelor's of Technology in
+          Information Technology. Looking forward to gaining industry-level
+          experience.
+        </div>
+        <div className='about-technologies'>
+          <div>Here are a few technologies I've been working with recently:</div>
+          <div className='d-flex gap-3'>
+            <ul>
+              <li>JavaScript (ES6+)</li>
+              <li>Node.js</li>
+            </ul>
+            <ul>
+              <li>React.js</li>
+              <li>UI Libraries</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
