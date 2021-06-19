076c4638c33621be64405af069a204a85ca4868d
Manan Doshi <manandoshi1301@gmail.com>
2021-06-19 17:06:47 +0530

    WorkIcon Configured

--- src/components/layout/work/Work.js
+++ src/components/layout/work/Work.js
@@ -10,7 +10,7 @@ const Work = () => {
     return (
         <div className='container work' id='work'>
             <div className='position-relative'>
-                {/* <WorkIcon /> */}
+                <WorkIcon />
                 <h1 className='display-1 text-center work-head' data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-duration='1000' data-aos-delay='500'>Wanna See my Work?!</h1>
             </div>
             <WorkCarousel />
