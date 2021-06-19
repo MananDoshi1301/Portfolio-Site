e6984b9e0ed06555739e9d8274c09eb728118045
Manan Doshi <manandoshi1301@gmail.com>
2021-06-19 14:25:41 +0530

    Scroll hook created and configured in designated files

--- src/components/layout/animationRevolve/AnimationRevolve.js
+++ src/components/layout/animationRevolve/AnimationRevolve.js
@@ -1,42 +1,12 @@
 import React,{useState, useEffect} from 'react';
 import "./AnimationRevolve.css";
-import img from '../../images/revolve2.png';
+import img from '../../images/revolve8.png';
+import useScrollStatus from '../../hooks/useScrollStatus';
 
 
-const AnimationRevolve = () => {
-  const [scrollPosition, setScrollPosition] = useState(0);
+const AnimationRevolve = () => {  
 
-  const getDocHeight = () => {
-    return Math.max(
-      document.body.scrollHeight, document.documentElement.scrollHeight,
-      document.body.offsetHeight, document.documentElement.offsetHeight,
-      document.body.clientHeight, document.documentElement.clientHeight
-    );
-  }
-
-  const calculateScrollDistance = () => {
-    const scrollTop = window.pageYOffset; // how much the user has scrolled by
-    const winHeight = window.innerHeight;
-    const docHeight = getDocHeight();
-    const totalDocScrollLength = docHeight - winHeight;    
-    const scrollValue = Math.floor(scrollTop / totalDocScrollLength * 100);
-    
-    setScrollPosition(scrollValue); 
-  }
-
-  const listenToScrollEvent = () => {
-    document.addEventListener("scroll", () => {
-      requestAnimationFrame(() => {
-        // this.calculateScrollDistance();
-        calculateScrollDistance();
-      });
-    });
-  }
-
-  useEffect(()=>{
-    listenToScrollEvent();
-  },[scrollPosition])
-
+  const scrollPosition = useScrollStatus();
   return (
     <div>
       <div className='revolve-mainDiv'>
--- src/components/layout/progressbar/ProgressBar.js
+++ src/components/layout/progressbar/ProgressBar.js
@@ -1,38 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import useScrollStatus from '../../hooks/useScrollStatus';
 
 const ProgressBar = () => {
-  const [scrollPosition, setScrollPosition] = useState(0);
-
-  const getDocHeight = () => {
-    return Math.max(
-      document.body.scrollHeight, document.documentElement.scrollHeight,
-      document.body.offsetHeight, document.documentElement.offsetHeight,
-      document.body.clientHeight, document.documentElement.clientHeight
-    );
-  }
-
-  const calculateScrollDistance = () => {
-    const scrollTop = window.pageYOffset; // how much the user has scrolled by
-    const winHeight = window.innerHeight;
-    const docHeight = getDocHeight();
-    const totalDocScrollLength = docHeight - winHeight;
-    const scrollValue = Math.floor(scrollTop / totalDocScrollLength * 100);
-
-    setScrollPosition(scrollValue);
-  }
-
-  const listenToScrollEvent = () => {
-    document.addEventListener("scroll", () => {
-      requestAnimationFrame(() => {
-        // this.calculateScrollDistance();
-        calculateScrollDistance();
-      });
-    });
-  }
-
-  useEffect(() => {
-    listenToScrollEvent();
-  }, [scrollPosition])
+  const scrollPosition = useScrollStatus();
+  
   const style = {
     position: 'fixed',
     top: 0,
