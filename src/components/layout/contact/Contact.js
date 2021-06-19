c5448ab634933c291437e66861ba828fe12bbdac
Manan Doshi <manandoshi1301@gmail.com>
2021-06-19 17:57:42 +0530

    New Connect Animation Configured!

--- src/components/layout/contact/Contact.js
+++ src/components/layout/contact/Contact.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ContactForm from './ContactForm';
 import './Contact.css';
 import ContactAnimation from './ContactAnimation';
+import ConnectAnimation from './connectAnimation/ConnectAnimation';
 
 const Contact = () => {
   return (
-    <div className='container contact' id='contact'>
+    <div className='container contact' style={{marginTop:'7rem'}}  id='contact'>
+      <ConnectAnimation />
       <div className='display-1 h1 text-end contact-title'>
         Connect!
       </div>
