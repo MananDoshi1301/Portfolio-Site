f2667acf2cfb657c034f09b471bdf678666b7a51
Manan Doshi <manandoshi1301@gmail.com>
2022-01-05 12:30:30 +0530

    Updated about content

--- src/components/layout/profile/AboutContent.js
+++ src/components/layout/profile/AboutContent.js
@@ -14,21 +14,22 @@ const AboutContent = () => {
     >
       <div className='w-90 about-content d-flex flex-column justify-content-around'>
         <div className='about-content-main'>
-          Enthusiastic about Web Architectures, Machine Learning, and Blockchain
-          Technologies, I am currently pursuing a Bachelor's of Technology in
+          Enthusiastic about Data analysis, its feature branches, Machine Learning and Statistics, I am currently pursuing a Bachelor's of Technology in
           Information Technology. Looking forward to gaining industry-level
-          experience.
+          experience in a Data Driven field.
+          I also expertise in web development primarily with React!
         </div>
+        <br />
         <div className='about-technologies'>
           <div>Here are a few technologies I've been working with recently:</div>
           <div className='d-flex gap-5 fs-5'>
             <ul className='about-technologies-ul'>
-              <li>JavaScript (ES6+)</li>
-              <li>Node.js</li>
+              <li>R & Python</li>
+              <li>IBM SPSS</li>
             </ul>
             <ul className='about-technologies-ul'>
               <li>React.js</li>
-              <li>UI Libraries</li>
+              <li>ML/DS</li>
             </ul>
           </div>
         </div>
