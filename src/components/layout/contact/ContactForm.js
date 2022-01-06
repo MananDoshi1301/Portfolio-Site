cd7c48077e47ada43984fb7acee4d3ecbfda1887
Manan Doshi <manandoshi1301@gmail.com>
2022-01-06 13:15:22 +0530

    Updated content in contact form

--- src/components/layout/contact/ContactForm.js
+++ src/components/layout/contact/ContactForm.js
@@ -26,7 +26,8 @@ const ContactForm = () => {
                 </div>
             </div>
             <div className="contact-form-content">
-                <span className='contact-form-content-hi'>Say Hi!<span className="loader__dot">.</span><span className="loader__dot">.</span><span className="loader__dot">.</span></span><br />I love creating, designing and working on Web Architectures. I have the ability to teach. Currently I am finding myself a space in Tech Industry and exploring various technologies. Connect with me!
+                <span className='contact-form-content-hi'>Say Hi!<span className="loader__dot">.</span><span className="loader__dot">.</span><span className="loader__dot">.</span></span><br />
+                I love working on Web Architectures and Data Architectures. I have the ability to teach. I spend my remaining time in music, sports, personal fitness and outdoors!
             </div>
             <div className="contact-form-divider d-flex justify-content-center"><div></div></div>
             <div className="row contact-form-row text-center">
